d20d27e327345f859a4484935d6627cdf03ac71f
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 12:55:16 +0600

    Добавьте файлы проекта.

---
+++ CW_1.4_25.06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//4.	С некоторого момента прошло 234 дня. Сколько полных недель прошло за этот период?
+namespace CW_1._4_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("С некоторого момента прошло 234 дня.");
+            var day = 234;
+            var week = day/7;
+            Console.Write("Полных недель прошло за этот период: ");
+            Console.WriteLine(week);
+            Console.ReadKey();
+        }
+    }
+}
